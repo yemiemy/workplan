4fc3119357df85514b729959332d02be75868ae5
yemiemy <rasholayemi@gmail.com>
2022-04-09 22:41:17 +0000

    configured the cronjob to work

--- core/management/commands/updateworkers.py
+++ core/management/commands/updateworkers.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from core.models import Worker, Shift
+from django.utils import timezone
 
 class Command(BaseCommand):
     help = "Update workers's availability everyday"
@@ -9,8 +10,11 @@ class Command(BaseCommand):
         for worker in worker_qs:
             worker.is_available = True
             worker.save()
-        
+
         shift_qs = Shift.objects.all()
         for shift in shift_qs:
             shift.workers.clear()
             shift.save()
+        print("All updates done to shifts and workers.")
+        time = timezone.now().strftime('%X')
+        self.stdout.write("All updates done to shifts and workers at %s" % time)
