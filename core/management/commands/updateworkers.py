c1c08ba24f6d7d5a7fab0fcc6c04727e3e7f18a6
Yemiemy <rasholayemi@gmail.com>
2022-04-09 20:31:10 +0100

    updated the management command `updateworkers`

--- core/management/commands/updateworkers.py
+++ core/management/commands/updateworkers.py
@@ -1,10 +1,16 @@
 from django.core.management.base import BaseCommand
-from django.utils import timezone
+from core.models import Worker, Shift
 
 class Command(BaseCommand):
     help = "Update workers's availability everyday"
 
     def handle(self, *args, **kwargs):
-        time = timezone.now().strftime('%X')
-        self.stdout.write("It's now %s" % time)
-        print("This works now")
+        worker_qs = Worker.objects.all()
+        for worker in worker_qs:
+            worker.is_available = True
+            worker.save()
+        
+        shift_qs = Shift.objects.all()
+        for shift in shift_qs:
+            shift.workers.clear()
+            shift.save()
